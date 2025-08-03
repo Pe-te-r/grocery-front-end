a7eee8253c993fa61e316d3fe7b1e912d3fd0b1e
Pe-te-r <mburupeter8526@gmail.com>
2025-08-03 22:46:50 +0300

    working

--- src/routes/(auth)/login.tsx
+++ src/routes/(auth)/login.tsx
@@ -110,6 +110,7 @@ function LoginPage() {
                   href="#"
                   className="inline-flex items-center rounded-md bg-black px-3 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-800 transition-colors"
                 >
+                  
                   <svg className="-ml-0.5 mr-2 h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
                     <path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z" />
                   </svg>
