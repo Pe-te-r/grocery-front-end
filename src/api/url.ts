810c32a798a9c7d89bd984d294acbca5be040f2c
Pe-te-r <mburupeter8526@gmail.com>
2025-08-04 01:40:55 +0300

    working api

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,5 +1,6 @@
 // export const url = 'http://localhost:4001'
-export const url = 'http://192.168.0.101:4001'
+// export const url = 'http://192.168.0.101:4001'
+export const url = 'https://nest-api-trial.onrender.com'
 // export const url = 'http://192.168.137.56:4001'
 // export const url = 'http://192.168.137.56:4001'
 // export const url = 'http://172.20.0.1:4001'
